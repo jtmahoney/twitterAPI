a14078d023e6408777369a843e13556e5d59819f
jtmahoney <jtmahoney@users.noreply.github.com>
2017-07-12 17:15:09 -0700

    Update twitterapi.py

--- twitterapi.py
+++ twitterapi.py
@@ -11,10 +11,10 @@ handle="@realDonaldTrump"
 #handle="@BarakObama"
 #handle="@DNC"
 
-twitter_consumer_key = 'P06NDrwliqLmANArc7GKwTgpI'
-twitter_consumer_secret = 'kx0sfDGOznmTGAWL7vfwdBEbuyJOAvDwPxiWZ2DRjwYZU1E8H4'
-twitter_access_token = '873284921954926592-B8952lHPl5LIMci85X31ZqNVUDUOiyt'
-twitter_access_secret = 'ESgDI70IEflmY3NCBjgwWkM9xl6n9Hi4XriAAPzkFa3we'
+twitter_consumer_key = ''
+twitter_consumer_secret = ''
+twitter_access_token = ''
+twitter_access_secret = ''
 twitter_api = twitter.Api(consumer_key=twitter_consumer_key, consumer_secret=twitter_consumer_secret, access_token_key=twitter_access_token, access_token_secret=twitter_access_secret)
 statuses = twitter_api.GetUserTimeline(screen_name=handle, count=200, include_rts=False)
 
